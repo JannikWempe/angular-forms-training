fb8bf52a7ebd421b006070b6153f42678f594a53
Jannik Wempe <jannik.wempe@iteratec.com>
2021-07-07 12:51:58 +0200

    remove default value accessor

--- src/app/auth/textual-input/textual-input.component.ts
+++ src/app/auth/textual-input/textual-input.component.ts
@@ -1,6 +1,8 @@
-import {Component, forwardRef, Input} from '@angular/core';
-import {DefaultValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+import {Component, Input} from '@angular/core';
 
+// TODO: make this component usable within forms without implementing ControlValueAccessor
+// [ ] this component can be used within a form
+// [ ] this component remains unchanged
 @Component({
   selector: 'app-textual-input',
   templateUrl: './textual-input.component.html',
