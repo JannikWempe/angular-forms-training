55ec645e37ace2a9d53037739780a85050b921a4
Jannik Wempe <jannik.wempe@iteratec.com>
2021-07-07 13:00:23 +0200

    remove textual input

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { ReactiveFormsModule } from "@angular/forms";
-import { TextualInputComponent } from './textual-input/textual-input.component';
 
 
 @NgModule({
   declarations: [
     SignupFormComponent,
-    TextualInputComponent,
   ],
   exports: [
     SignupFormComponent
