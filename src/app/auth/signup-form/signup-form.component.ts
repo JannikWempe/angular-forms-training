250d80b1c8f8fd54755beb4c1aa88f5ab31fc3d3
Jannik Wempe <jannik.wempe@iteratec.com>
2021-07-07 13:04:06 +0200

    remove custom validator

--- src/app/auth/signup-form/signup-form.component.ts
+++ src/app/auth/signup-form/signup-form.component.ts
@@ -17,20 +17,16 @@ export class SignupFormComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {}
 
-  private static isForbiddenUsername(control: FormControl): Record<'isForbiddenUsername', true> | null {
-    if (USERNAME_BLACKLIST.includes(control.value)) {
-      return { isForbiddenUsername: true }
-    }
-    return null
-  }
-
   ngOnInit() {
     this.initForm();
   }
 
   initForm() {
     this.form = this.fb.group({
-      'username': ['', [Validators.required, SignupFormComponent.isForbiddenUsername]],
+      // TODO: create a custom validator "isForbiddenUsername"
+      // [ ] username is invalid if name is listed in USERNAME_BLACKLIST
+      // [ ] Error "Username xyz is not allowed." is only shown if custom validator is invalid
+      'username': ['', Validators.required],
       'name': this.fb.group({
         'first': [''],
         'last': ['', Validators.required]
